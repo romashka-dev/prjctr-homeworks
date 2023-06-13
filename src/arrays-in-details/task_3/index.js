bf960ccf876743c2bcb9df05429c2fd9bd5a5959
rh-privat <rh-privat@4938.local>
2023-06-13 16:23:14 +0300

    [arrays-in-details]: homework 1 | fix 2

--- src/arrays-in-details/task_3/index.js
+++ src/arrays-in-details/task_3/index.js
@@ -4,7 +4,7 @@ const resultsArray = [1, 2, [3, [4]]];
 let productOfArray = resultsArray;
 
 // тут ваш код...
-productOfArray = productOfArray.flat(2);
+productOfArray = productOfArray.flat(Number(Infinity));
 productOfArray = productOfArray.reduce((a,b) => a * b);
 
 console.log(productOfArray); // 24
