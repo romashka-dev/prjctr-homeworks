a5cacaa7b360427b0acb84d42343204ae8cfd64a
rh-privat <rh-privat@4938.local>
2023-06-16 23:33:23 +0300

    [objects-and-functions]: homework 2, without task 3

---
+++ src/objects-and-functions/homework-2.js
@@ -0,0 +1,84 @@
+// --- TASK 1 --- //
+// Задача про обчислення різниці часу
+// Напишіть функцію яка буде буде приймати 3 параметри
+// — початкову дату (string)
+// — кінцеву дату (string)
+// — розмірність ('days', 'hours', 'minutes', seconds)
+// Функція повертатиме часовий період між цими датами згідно розмірності.
+// Також вкажіть значення по замовчуванню для всіх цих параметрів (на ваш вибір).
+// Функція має коректно працювати навіть якщо початкова дата пізніше ніж кінцева дата.
+
+const durationBetweenDates = (dateFrom, dateTo) => {
+    const start = new Date(dateFrom);
+    const end = new Date(dateTo);
+
+    const timeDiff = Math.abs(end - start);
+    const durationInDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    return `${durationInDays} days`;
+};
+
+// Test of durationBetweenDates():
+// console.log(durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')); // поверне '362 days'
+// console.log(durationBetweenDates('01 Jan 2014', '03 Feb 2023', 'days')); // поверне '3320 days'
+// console.log(durationBetweenDates('01 Jan 2018', '03 Feb 2023', 'days')); // поверне '1859 days'
+
+
+
+
+
+// --- TASK 2 --- //
+// Задача про перетворення об'єкту
+// Допустимо у вас є об'єкт, у якому кожен ключ - це назва товару (одним словом), а значення - його ціна.
+// Напишіть функцію яка буде всі ключі переводити у нижній регістр, а всі ціни буде заокруглювати до двох знаків після коми.
+
+const priceData = {
+    Apples: '23.4',
+    BANANAS: '48',
+    oRAngGEs: '48.7584'
+};
+
+const priceData2 = {
+    SnEakerS_AirForce_320: '599.45',
+    SnEakerS_RunnING_110: '428.1',
+    snEakerS_runnING_150: '499.7584'
+};
+
+const optimizer = (data) => {
+    const updatedData = {};
+
+    for (const item in data) {
+        const lowerCaseKey = item.toLowerCase();
+        const roundedPrice = parseFloat(data[item]).toFixed(2);
+        updatedData[lowerCaseKey] = roundedPrice;
+    }
+
+    return updatedData;
+};
+
+let updatedPriceData = optimizer(priceData);
+let nikePriceData = optimizer(priceData2);
+// Test of durationBetweenDates():
+// console.log(updatedPriceData) // { apples: '23.40', bananas: '48.00', oranges: '48.76' }
+// console.log(nikePriceData) // { sneakers_airforce_320: '599.45', sneakers_running_110: '428.10', sneakers_running_150: '499.76' }
+
+
+
+
+
+// --- TASK 3 --- //
+// Задача про рекурсію та ітерацію
+// Напишіть:
+// — функцію яка рекурсивно буде знаходити суму всіх непарних додатних чисел до якогось числа.
+const recursiveOddSumTo = (number) => {
+};
+
+// console.log(recursiveOddSumTo(3)) // 1
+// console.log(recursiveOddSumTo(10)) // 25
+
+// — функцію яка ітеративно (в циклі) буде знаходити суму всіх непарних додатних чисел до якогось числа.
+const iterativeOddSumTo = (number) => {
+};
+
+// console.log(iterativeOddSumTo(1)) // 1       
+// console.log(iterativeOddSumTo(10)) // 25
