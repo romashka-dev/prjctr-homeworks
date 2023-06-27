20e7d0a1efdf9a9d526c4aa1b35a3750cc3eea26
rh-privat <rh-privat@4938.local>
2023-06-27 10:27:08 +0300

    [objects-and-functions]: homework 2 | upd 4

--- src/objects-and-functions/homework-2.js
+++ src/objects-and-functions/homework-2.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // --- TASK 1 --- //
 // Задача про обчислення різниці часу
 // Напишіть функцію яка буде буде приймати 3 параметри
@@ -7,7 +9,6 @@
 // Функція повертатиме часовий період між цими датами згідно розмірності.
 // Також вкажіть значення по замовчуванню для всіх цих параметрів (на ваш вибір).
 // Функція має коректно працювати навіть якщо початкова дата пізніше ніж кінцева дата.
-
 const durationBetweenDates = (dateFrom = new Date('31 Jan 2022'), dateTo = new Date(), dimension = 'days') => {
     const start = new Date(dateFrom);
     const end = new Date(dateTo);
@@ -17,15 +18,10 @@ const durationBetweenDates = (dateFrom = new Date('31 Jan 2022'), dateTo = new D
         return 'Invalid date';
     }
 
-    const timeDiffSeconds = Math.abs(end.getTime() - start.getTime()) / 1000;
-    const timeDiffMinutes = Math.abs(end.getTime() - start.getTime()) / (1000 * 60);
-    const timeDiffHours = Math.abs(end.getTime() - start.getTime()) / (1000 * 60 * 60);
-    const timeDiffDays = Math.abs(end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24);
-
-    const durationInSeconds = Math.ceil(timeDiffSeconds);
-    const durationInMinutes = Math.ceil(timeDiffMinutes);
-    const durationInHours = Math.ceil(timeDiffHours);
-    const durationInDays = Math.ceil(timeDiffDays);
+    const durationInSeconds = Math.ceil(Math.abs(end.getTime() - start.getTime()) / 1000);
+    const durationInMinutes = Math.ceil(Math.abs(end.getTime() - start.getTime()) / (1000 * 60));
+    const durationInHours = Math.ceil(Math.abs(end.getTime() - start.getTime()) / (1000 * 60 * 60));
+    const durationInDays = Math.ceil(Math.abs(end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24));
 
     switch (dimension) {
         case 'days':
@@ -40,7 +36,6 @@ const durationBetweenDates = (dateFrom = new Date('31 Jan 2022'), dateTo = new D
             return 'Invalid dimension';
     }
 };
-
 // Test of durationBetweenDates():
 // console.log(durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds')); // поверне '86400 seconds'
 // console.log(durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')); // поверне '362 days'
@@ -53,7 +48,6 @@ const durationBetweenDates = (dateFrom = new Date('31 Jan 2022'), dateTo = new D
 // Задача про перетворення об'єкту
 // Допустимо у вас є об'єкт, у якому кожен ключ - це назва товару (одним словом), а значення - його ціна.
 // Напишіть функцію яка буде всі ключі переводити у нижній регістр, а всі ціни буде заокруглювати до двох знаків після коми.
-
 const priceData = {
     Apples: '23.4',
     BANANAS: '48',
@@ -72,7 +66,6 @@ const optimizer = (data) => {
 };
 
 let updatedPriceData = optimizer(priceData);
-
 // Test of durationBetweenDates():
 // console.log(updatedPriceData) // { apples: '23.40', bananas: '48.00', oranges: '48.76' }
 
@@ -85,19 +78,14 @@ let updatedPriceData = optimizer(priceData);
 // Напишіть:
 // — функцію яка рекурсивно буде знаходити суму всіх непарних додатних чисел до якогось числа.
 const recursiveOddSumTo = (number) => {
-    const current = 1;
-
-    const oddSumTo = (current) => {
-        if (current > number) {
-            return 0;
-        }
-
-        return current + oddSumTo(current + 2);
-    };
-    
-    return oddSumTo(current);
+    if (number <= 0) {
+        return 0;
+    } else if (number % 2 !== 0) {
+        return number + recursiveOddSumTo(number - 2);
+    } else {
+        return recursiveOddSumTo(number - 1);
+    }
 };
-
 // console.log(recursiveOddSumTo(1)) // 1
 // console.log(recursiveOddSumTo(10)) // 25
 
@@ -111,6 +99,5 @@ const iterativeOddSumTo = (number) => {
   
     return sum;
 };
-
 // console.log(iterativeOddSumTo(1)) // 1       
 // console.log(iterativeOddSumTo(10)) // 25
